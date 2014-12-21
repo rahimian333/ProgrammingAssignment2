b3a7082be9d06c1322e4805eaa18a8434ad4a430
rahimian333 <rahimian333@yahoo.com>
2014-12-21 14:05:30 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,80 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+This function creates a matrix with the capability of computing the inverse of the matrix.
+makeCacheMatrix <- function (x = matrix ()) {
+	inverse.x <- NULL
+	set <- function (y) {
+	x <<- y
+	inverse.x <<- NULL
+	}
+	Get <<- function () x
+	Set.inverse <- function (inverse) inverse.x <<- inverse
+	Get.inverse <- function () inverse.x
+	List (set = set, get = get,
+	Set.inverse = set.inverse,
+	Get.inverse = get.inverse)
 }
-
-
-## Write a short comment describing this function
-
+This function gets the stored cached inverse matrix, and if the cached inverse is not available, this function calculates it.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inverse <- x$get.inverse()
+        if(!is.null(inverse)) {
+                message("getting cached data")
+                return(inverse)
+        }
+        data <- x$get.inverse()
+        inverse <- inverse(data, ...)
+        x$set.inverse(x)
+        x
 }
+
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function (x = matrix ()) {
+  inverse.x <- NULL
+  set <- function (y) {
+    x <<- y
+    inverse.x <<- NULL
+  }
+  Get <<- function () x
+  Set.inverse <- function (inverse) inverse.x <<- inverse
+  Get.inverse <- function () inverse.x
+  List (set = set, get = get,
+        Set.inverse = set.inverse,
+        Get.inverse = get.inverse)
+} 
+cacheSolve <- function(x, ...) {
+  inverse <- x$get.inverse()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- x$get.inverse()
+  inverse <- inverse(data, ...)
+  x$set.inverse(x)
+  x
+} 
+makeCacheMatrix <- function (x = matrix ()) {
+  inverse.x <- NULL
+  set <- function (y) {
+    x <<- y
+    inverse.x <<- NULL
+  }
+  Get <<- function () x
+  Set.inverse <- function (inverse) inverse.x <<- inverse
+  Get.inverse <- function () inverse.x
+  List (set = set, get = get,
+        Set.inverse = set.inverse,
+        Get.inverse = get.inverse)
+} 
+cacheSolve <- function(x, ...) {
+  inverse <- x$get.inverse()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- x$get.inverse()
+  inverse <- inverse(data, ...)
+  x$set.inverse(x)
+  x
+} 
